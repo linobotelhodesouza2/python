e17509ba56927ef63029df063a4d638e22e7525f
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-01 18:43:34 -0300

    Add files via upload

---
+++ aula06a.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite o primeiro numero : '))
+n2 = int(input('Digite o segundo numero : '))
+s = n1+n2
+#print('A soma é ',s)
+#print('A soma entre ',n1,'e',n2,'vale',s) python sintax versao 2
+#print('A soma entre {0} e {1} vale {2}'.format(n1, n2, s))
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
---
+++ aula06b.py
@@ -0,0 +1,4 @@
+n = input('Digite um valor: ')
+#print(type(n))
+print(n.isnumeric())
+
