0d848da821dfd2186830f6d67dea6bb1bdc1627e
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-03 20:01:34 -0300

    Add files via upload

---
+++ aula07f.py
@@ -0,0 +1,9 @@
+#FAÇA UM PROGRAMA QUE LEIA UM NUMERO INTEIRO QUALQUER E MOSTRE NA TELA A SUA TABUADA.
+n = int(input('Digite um numero: '))
+t = n*1
+print('O numero digitado foi {}, sua tabuada é \n {} '.format(n,t))
+
+
+
+
+
