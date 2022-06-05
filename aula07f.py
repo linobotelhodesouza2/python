6f64b6d7d22070f885efb25f1a2f093a43fecd96
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-04 21:10:57 -0300

    Add files via upload

--- aula07f.py
+++ aula07f.py
@@ -1,9 +1,15 @@
 #FAÇA UM PROGRAMA QUE LEIA UM NUMERO INTEIRO QUALQUER E MOSTRE NA TELA A SUA TABUADA.
 n = int(input('Digite um numero: '))
-t = n*1
-print('O numero digitado foi {}, sua tabuada é \n {} '.format(n,t))
-
-
-
-
-
+#print('O numero digitado foi {}, sua tabuada é \n {} x {} = {} '.format(n, n, 1, n * 1 ))
+print('-' * 10)
+print('{} x {} = {} '.format(n, 1, n * 1))
+print('{} x {} = {} '.format(n, 2, n * 2))
+print('{} x {} = {} '.format(n, 3, n * 3))
+print('{} x {} = {} '.format(n, 4, n * 4))
+print('{} x {} = {} '.format(n, 5, n * 5))
+print('{} x {} = {} '.format(n, 6, n * 6))
+print('{} x {} = {} '.format(n, 7, n * 7))
+print('{} x {} = {} '.format(n, 8, n * 8))
+print('{} x {} = {} '.format(n, 9, n * 9))
+print('{} x {} = {} '.format(n, 10, n * 10))
+print('-' * 10)
