a886fbe8dafba6aea4454a4be91aff69f8bbb74c
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-10 22:55:15 -0300

    Add files via upload

--- aula09a.py
+++ aula09a.py
@@ -1,3 +1,10 @@
 '''
 manipulando textos
 '''
+
+s = 'Curso em Video Python'
+print(s[9::3]) #pula de tres em tres e a primeira posicao é em V
+print(s.count('o', 0, 13)) #mostra o valor 1 pois procura a letra 'o' e so tem uma em posicao 4 e a posicao 13 nao vale pois antecipa uma ficando na posicao 12
+print(s.find('deo')) #mostra na tela aonde comeca a palavra procurada no caso posicao 11
+print(s.find('Android')) #mostra um valor negativo, pois nao tem a palavra procurada
+print('Curso'in(s)) #mostra se a condição é verdadeira
