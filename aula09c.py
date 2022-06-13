3a4427c37cffc312fbb47bcf01249413cc29dda6
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-13 18:12:19 -0300

    Add files via upload

---
+++ aula09c.py
@@ -0,0 +1,16 @@
+'''
+DESAFIO
+FAÇA UM PROGRAMA QUE LEIA UM NUMERO DE 0 A 9999 E MOSTRE NA TELA CADA UM DOS DIGITOS SEPARADOS.
+EX: DIGITE UM NUMERO : 1834
+UNIDADE:4
+DEZENA:3
+CENTENA:8
+MILHAR:1
+'''
+n = int(input('Digite um numero qualquer: '))
+
+print('Unidade: {} \nDezena: {} \nCentena: {} \nMilhar: {}'.format())
+
+
+
+
