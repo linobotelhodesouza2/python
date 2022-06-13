6ef0b4653ded157d7c67f1f60ede36b8da4dd1d5
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-13 18:34:27 -0300

    Add files via upload

---
+++ aula09d.py
@@ -0,0 +1,7 @@
+'''
+DESAFIO
+CRIE UM PROGRAMA QUE LEIA O NOME DE UMA CIDADE E DIGA SE ELA COMEÇA OU NAO COM O NOME 'SANTO'
+'''
+n = str(input('Digite o nome da cidade: '))
+print(n.find('santo')
+
