cf82bbea5f76f8c05a8a80d2f33303b97508e518
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-14 15:58:40 -0300

    Add files via upload

--- aula09b.py
+++ aula09b.py
@@ -6,15 +6,15 @@ CRIE UM PROGRAMA QUE LEIA O NOME COMPLETO DE UMA PESSOA E MOSTRE:
 - QUANTAS LETRAS AOTODO SEM CONSIDERAR ESPACOS
 - QUANTAS LETRAS TEM O PRIMEIRO NOME.
 '''
-n = str(input('Digite seu nome: '))
-print(n.upper())
-print(n.lower())
-print(len(n))
-print(len(n.rstrip()))
-print(n.split())
-print(len(n.split()))
-
-
+n = str(input('Digite seu nome completo: ')).strip()# usando o '.strip' direto aqui para eliminar os espaços no campo de nome,strip remove os espaços excedentes
+print('Analisando seu nome...')
+print('Seu nome em maiscula é {}'.format(n.upper()))
+print('Seu nome em minuscula é {}'.format(n.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(n) - n.count(' ')))# dessa forma o len conta quantas letras tem junto com o espaço porem eu uso o - n.count('  ') para retirar do contador o espaço atribuido ao nome
+print('Seu primeiro nome tem {} letras'.format(n.find(' ')))
+#outra forma de fazer
+separa = n.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
 
 
 
