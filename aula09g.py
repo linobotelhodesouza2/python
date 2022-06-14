981f7be43125f6848df83eb7bca234025cd22287
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-14 16:58:34 -0300

    Add files via upload

--- aula09g.py
+++ aula09g.py
@@ -7,8 +7,9 @@ PRIMEIRO:ANA
 ULTIMO:SOUZA
 '''
 n = str(input('Digite seu nome completo: ')).strip()
-nome = n.split()
+nome = n.split()# split fatia a str em forma de lista
 print('Prazer em te conhecer!')
 print('Seu primeiro nome é {}.'.format(nome[0]))
-print('Seu ultimo nome é {}.'.format(nome[len(nome)-1]))
-
+print('Seu ultimo nome é {}.'.format(nome[len(nome)-1]))# -1 é sempre o ultimo nome
+#Outra forma de se fazer sem o len
+print('Seu ultimo nome é {}.'.format(nome[-1]))
