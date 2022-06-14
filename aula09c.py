c6bbd9ee7b3a5f634df2198850b145d27ae5f386
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-14 16:10:40 -0300

    Add files via upload

--- aula09c.py
+++ aula09c.py
@@ -7,10 +7,16 @@ DEZENA:3
 CENTENA:8
 MILHAR:1
 '''
-n = int(input('Digite um numero qualquer: '))
-
-print('Unidade: {} \nDezena: {} \nCentena: {} \nMilhar: {}'.format())
-
+num = int(input('Digite um numero qualquer: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Analisando o numero {}'.format(num))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
 
 
 
