470808bef02c4f9395dd0af1303d711c964e5be5
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-07-31 14:16:08 -0300

    Add files via upload

--- desafio28_JogoAdivinhacao.py
+++ desafio28_JogoAdivinhacao.py
@@ -3,3 +3,10 @@ ESCREVA UM PROGRAMA QUE FAÇA O COMPUTADOR 'PENSAR' EM UM NUMERO INTEIRO ENTRE 0
 ESCOLHIDO PELO COMPUTADOR.
 O PROGRAMA DEVERÁ ESCREVER NA TELA SE O USUARIO VENCEU OU PERDEU.
 '''
+from random import randint
+num = int(input('DIgite um numero de 1 a 5: '))
+sorteio = randint(1, 5)
+if num == sorteio:
+    print('Voçê acertou, o numero digitado foi {} eu pensei no numero {}'.format(num, sorteio))
+else:
+    print('Voçê errou, voçê digitou {} e eu pensei no numero {}'.format(num, sorteio))
