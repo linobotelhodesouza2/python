9956c82e75ea85d76330032e8caafa27f6f8fb8d
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-07-31 14:38:30 -0300

    Add files via upload

--- desafio29_RadarEletronico.py
+++ desafio29_RadarEletronico.py
@@ -3,3 +3,9 @@ ESCREVA UM PROGRAMA QUE LEIA A VELOCIDADE DE UM CARRO.
 SE ELE ULTRAPASSAR 80KM/H, MOSTRE UMA MENSAGEM DIZENDO QUE ELE FOI MULTADO.
 A MULTA VAI CUSTAR R$7,00 POR CADA KM ACIMA DO LIMITE.
 '''
+car = int(input('Digite a velocidade do veiculo em Km: '))
+velMaior = (car - 80) * 7
+if car > 80:
+    print('Voçê atingiu a velocidade de {} Km, e será multado em R${:.2f} reais.'.format(car, velMaior))
+else:
+    print('Voçê esta na velocidade de {} Km e esta na velocidade permitida, siga assim.'.format(car, velMaior))
