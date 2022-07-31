4d7db3341ef487fad88af348646108c285cfcdd2
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-07-31 16:59:39 -0300

    Add files via upload

--- desafio33_MaiorEMenorValores.py
+++ desafio33_MaiorEMenorValores.py
@@ -1,3 +1,20 @@
 '''
 FAÇA UM PROGRAMA QUE LEIA TRÊS NUMEROS E MOSTRE QUAL É O MAIOR E QUAL É O MENOR.
 '''
+
+a = int(input('Digite o primeiro numero: '))
+b = int(input('Digite o segundo numero: '))
+c = int(input('Digite o terceiro numero: '))
+
+menor = a
+if b<a and b<c:
+    menor = b
+if c<a and c<b:
+    menor = c
+maior = a
+if b>a and b>c:
+    maior = b
+if c>a and c>b:
+    maior = c
+print('O menor foi {}'.format(menor))
+print('O maior foi {}'.format(maior))
