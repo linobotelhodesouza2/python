7d7178748275ea7c8137bd4d84a73f00eedfd201
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-07-31 19:13:41 -0300

    Add files via upload

---
+++ desafio36_CoresNoTerminal.py
@@ -0,0 +1,18 @@
+'''
+PADRAO ANSI ESCAPE SEQUENCE
+EX: STYLE  BACK
+\033[0: 33:  44  M
+       TEXT
+STYLE        TEXT           BACK
+
+0 NONE      30 BRANCO      40
+1 BOLD      31 VERMELHO    41
+4 UNDELINE  32 VERDE       42
+7 NEGATIVE  33 AMARELO     43
+            34 AZUL        44
+            35 PURPLE      45
+            36 AZUL CLARO  46
+            37 CINZA       47
+
+
+'''
