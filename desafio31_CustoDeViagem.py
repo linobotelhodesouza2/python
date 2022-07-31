924059a99126e690d0c3993bafdc9e5ae2051dd9
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-07-31 15:25:38 -0300

    Add files via upload

--- desafio31_CustoDeViagem.py
+++ desafio31_CustoDeViagem.py
@@ -2,3 +2,10 @@
 DESENVOLVA UM PROGRAMA QUE PERGUNTE A DISTÂNCIA DE UMA VIAGEM EM KM.
 CALCULE O PREÇO DA PASSAGEM , COBRANDO R$0,50 POR KM PARA VIAGENS DE ATÉ 200KM E R$0,45 PARA VIAGENS MAIS LONGAS.
 '''
+v = float(input('Digite a distância em Km da sua viagem: '))
+curta = v * 0.50
+longa = v * 0.45
+if v > 200:
+    print('Sua viagem teve {}Km e sua passagem será de R${:.2f} reais.'.format(v, longa))
+else:
+    print('Sua viagem teve {}Km e sua passagem será de R${:.2f} reais.'.format(v, curta))
