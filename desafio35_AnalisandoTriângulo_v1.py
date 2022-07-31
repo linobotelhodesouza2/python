db15b28fb9f25ab60650d12b8209d39e1809716b
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-07-31 17:16:00 -0300

    Add files via upload

--- desafio35_AnalisandoTriângulo_v1.py
+++ desafio35_AnalisandoTriângulo_v1.py
@@ -1,3 +1,11 @@
 '''
 DESENVOLVA UM PROGRAMA QUE LEIA O COMPRIMENTO DE TRÊS RETAS E DIGA AO USUÁRIO SE ELAS PODEM OU NÃO FORMAR UM TRIÂNGULO.
+triangulo e formado quando a soma dos dois lados for menor que a de um dos lados 
 '''
+a = float(input('Digite o primeiro segmento: '))
+b = float(input('Digite o segundo segmento: '))
+c = float(input('Digite o terceiro segmento: '))
+if a < b + c and b < c + a and c < a + b:
+    print('Os segmentos PODEM gerar um triângulo')
+else:
+    print('Os segmentos NÃO PODEM gerar um triângulo')
