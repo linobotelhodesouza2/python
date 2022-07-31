f1f302f9aa694a24752baa4d1c200864680e1b7f
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-07-31 15:10:59 -0300

    Add files via upload

--- desafio30_ParOuImpar.py
+++ desafio30_ParOuImpar.py
@@ -1,3 +1,9 @@
 '''
 CRIE UM PROGRMA QUE LEIA UM NUMERO INTEIRO E MOSTRE NA TELA SE ELE É PAR OU ÍMPAR.
 '''
+
+num = int(input('Digite um numero qualquer: '))
+if (num % 2 == 1): # o numero de resto 2  for igual a 1 será impar caso o contrario sera par
+    print('impar')
+else:
+    print('Par')
