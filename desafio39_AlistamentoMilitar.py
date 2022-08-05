142dc2c9aa78489f34f3bba3f2b66ffd4387219a
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-08-05 19:29:09 -0300

    Add files via upload

--- desafio39_AlistamentoMilitar.py
+++ desafio39_AlistamentoMilitar.py
@@ -9,4 +9,13 @@ from datetime import date# importa a biblioteca de data e o import date a data e
 nasc = int(input('Digite o ano de Nascimento: '))
 today = date.today().year# especifica que eu quero somente o ano .year
 idade = today - nasc
-print('{}'.format(idade))
+alist = 17
+
+if idade == alist:
+    print('Olá, voçê tem {} anos, e esta no periodo de alistamento militar, procure uma agência e se aliste.'.format(idade))
+elif idade > alist:
+    print('Olá, voçê tem {} anos e ja passou do periodo de alistamento militar em {} anos,\n'
+          'procure uma agência para regularizar sua situação.'.format(idade, idade - alist))
+elif idade < alist:
+    print('Olá, voçê tem {} anos e falta {} anos ainda, não esta no periodo de alistamento militar,\n'
+          'mais esta próximo.'.format(idade, alist - idade))
