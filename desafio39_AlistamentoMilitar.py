a1cf26f63c43e74e2626fa78a1b7cd274dc92d6b
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-08-05 19:16:24 -0300

    Add files via upload

---
+++ desafio39_AlistamentoMilitar.py
@@ -0,0 +1,12 @@
+'''
+FAÇA UM PROGRAMA QUE LEIA O ANO DE NASCIMENTO DE UM JOVEM E INFORME , DE ACORDO COM SUA IDADE:
+- SE ELE AINDA VAI SE ALISTAR AO SERVIÇO MILITAR.
+- SE É A HORA DE SE ALISTAR;
+- SE JÁ PASSOU DO TEMPO DO ALISTAMENTO
+ SEU PROGRAMA TAMBEMM DEVERA MOSTRAR O TEMPO QUE FALTA OU QUE PASSOU DO PRAZO.
+'''
+from datetime import date# importa a biblioteca de data e o import date a data especifica no caso today hoje
+nasc = int(input('Digite o ano de Nascimento: '))
+today = date.today().year# especifica que eu quero somente o ano .year
+idade = today - nasc
+print('{}'.format(idade))
