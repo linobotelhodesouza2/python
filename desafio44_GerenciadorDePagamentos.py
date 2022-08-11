c3d1d3ac6489e8e1f97bc50dc923b4a92fd2112b
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-08-11 19:34:25 -0300

    Add files via upload

--- desafio44_GerenciadorDePagamentos.py
+++ desafio44_GerenciadorDePagamentos.py
@@ -6,6 +6,7 @@ DE PAGAMENTO:
 - EM ATE 2X NO CARTAO : PRECO NORMAL
 - 3X OU MAIS NO CARTAO : 20% DE JUROS
 '''
+print('{:=^40}'.format(' LOJA '))# colocando o nome no meio
 prod = float(input('Digite o valor do Produto:R$'))
 descD = prod - (prod / 100) * 10
 descC = prod - (prod / 100) * 5
@@ -17,13 +18,17 @@ print('''\nEscolha a forma de Pagamento:
 [4] em mais de 3 vezes no Cartão com Juros
 ''')
 escolha = int(input('Sua Escolha de Pagamento:'))
-
+# A VISTA
 if escolha == 1:
     print('O valor do Produto escolhido é R${:.2f} reais, e avista ficará R${:.2f} reais, tanto no Cheque ou Dinheiro,'
           'Obrigado.'.format(prod, descD))
+
+# CARTAO A VISTA
 elif escolha == 2:
     print('O valor do Produto escolhido é R${:.2f} reais, e avista no Cartão ficará R${:.2f} reais, '
           'Obrigado.'.format(prod, descC))
+
+# EM 2x SEM JUROS
 elif escolha == 3:
     print('Voçê pode parcelar em até 2 Vezes sem Juros no Cartão \nQuantas vezes voçê quer parcelar?')
     parcela = int(input('Sua Escolha: '))
@@ -35,38 +40,13 @@ elif escolha == 3:
               'Obrigado.'.format(prod, prod / 2))
     elif parcela > 2:
         print('Voçê digitou um valor de parcela que não exite, tente novamente,Obrigado.')
+
+# EM MAIS DE 3x COM JUROS
 elif escolha == 4:
     print('Voçê pode parcelar em até 10 vezes com juros no Cartão\nQuantas vezes voçê quer parcelar?')
-    parcela = int(input('Sua escolha: '))
-    if parcela == 1:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 1 vez no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros))
-    elif parcela == 2:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 2 vezes no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros / 2))
-    elif parcela == 3:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 3 vezes no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros / 3))
-    elif parcela == 4:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 4 vezes no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros / 4))
-    elif parcela == 5:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 5 vezes no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros / 5 ))
-    elif parcela == 6:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 6 vezes no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros / 6))
-    elif parcela == 7:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 7 vezes no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros / 7))
-    elif parcela == 8:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 8 vezes no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros / 8))
-    elif parcela == 9:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 9 vezes no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros / 9))
-    elif parcela == 10:
-        print('O valor do Produto escolhido é R${:.2f} reais, em 10 vezes no Cartão será de R${:.2f} reais, '
-              'Obrigado.'.format(prod, juros / 10))
-    elif parcela > 10:
+    totalparc = int(input('Sua escolha em parcelas: '))
+    if totalparc > 10:
         print('Voçê digitou um valor de parcela que não exite, tente novamente,Obrigado.')
+    else:
+        print('Sua compra será parcelada em {}x de R${:.2f} com JUROS'.format(totalparc, juros / totalparc))
+        print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(prod, juros))
