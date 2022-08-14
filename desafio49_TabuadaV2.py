6de790795b23d6d9d8edaa3589a0d809068a7a88
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-08-14 10:58:13 -0300

    Add files via upload

--- desafio49_TabuadaV2.py
+++ desafio49_TabuadaV2.py
@@ -3,5 +3,5 @@ REFAÇA O DESAFIO 009, MOSTRANDO A TABUADA DE UM NUMERO QUE O USUARIO ESCOLHER, 
 UTILIZANDO O LAÇO FOR
 '''
 n = int(input('Digite um numero: '))
-for c in range( n * 0, 11, +1):
-    print('{} x {} = {}'.format(n, c, (n * c)))
+for c in range(n * 0, 11, 1):
+    print('{} x {:2} = {}'.format(n, c, n * c))
