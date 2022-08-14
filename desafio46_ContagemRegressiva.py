eb53c9f62742df432c534356cc174d476ea8161e
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-08-13 23:23:38 -0300

    Add files via upload

--- desafio46_ContagemRegressiva.py
+++ desafio46_ContagemRegressiva.py
@@ -2,4 +2,12 @@
 FAÇA  UM PROGRAMA QUE MOSTRE NA TELA UMA CONTAGEM REGRESSIVA PARA O ESTOURO DE FOGOS DE ARTIFICIO , INDO DE 10 ATÉ 0,
 COM UMA PAUSA DE 1 SEGUNDO ENTRE ELES.
 '''
-cont =
+import time
+t = 1
+time.sleep(t)
+for c in range(10, -1, -1):
+    t = 1
+    time.sleep(t)
+    print(c)
+    if c == 0:
+        print('\U0001F4A5 CHAZAAAANNN!!!!!')#codigo unicode explosão
