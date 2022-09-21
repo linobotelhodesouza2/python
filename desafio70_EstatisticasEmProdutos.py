0c6c6923b18cb84c85ec4596b6eeecd3ca4c5237
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-09-21 18:38:49 -0300

    Add files via upload

--- desafio70_EstatisticasEmProdutos.py
+++ desafio70_EstatisticasEmProdutos.py
@@ -5,3 +5,28 @@ A- QUAL É O TOTAL GASTO NA COMPRA.
 B- QUANTOS PRODUTOS CUSTAM MAIS DE 1000.
 C- QUAL É O NOME DO PRODUTO MAIS BARATO.
 '''
+print('-' * 10, 'LEITOR DE PRODUTOS', '-' * 10)
+cont = 0
+while True:
+    nome = str(input('Digite o nome do produto: ')).upper()
+    preco = int(input('Digite o valor do produto:R$ '))
+
+    resp = ' '
+    while resp not in 'SN':
+        print('-' * 50)
+        resp = str(input('Deseja continuar cadastrando [S/N]: ')).strip().upper()[0]
+        if resp == 'S':
+            cont += 1
+
+    if resp == 'N':
+        print('Obrigado.')
+        cont += 1
+        break
+
+
+print('-' * 50)
+print(f'\nVoçê cadastrou {cont} produto(s).')
+print('-' * 50)
+print(f'Produto: ........................{nome}')
+print(f'Preço: ..........................R${preco}')
+print('-' * 50)
