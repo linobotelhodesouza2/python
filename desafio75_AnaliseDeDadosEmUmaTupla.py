e02d3842930c6c499d7f55a95aeed6e9d13cdc8f
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-10-12 21:48:54 -0300

    Add files via upload

---
+++ desafio75_AnaliseDeDadosEmUmaTupla.py
@@ -0,0 +1,22 @@
+'''
+DESENVOLVA UM PROGRAMA QUE LEIA QUATRO VALORES PELO TECLADO E GUARDE-OS EM UMA TUPLA.NO FINAL,MOSTRE:
+A- QUANTAS VEZES APARECEU O VALOR 9
+B- EM QUE POSIÇÃO FOI DIGITADO O PRIMEIRO VALOR 3
+C- QUAIS FORAM OS NUMEROS PARES
+'''
+num = (int(input('Digite um numero: ')),
+       int(input('Digite outro numero: ')),
+       int(input('Digite mais um numero: ')),
+       int(input('Digite o ultimo numero: ')))
+print('Voçê digitou os valores:', end='')
+for numeros in num:
+    print(f' {numeros}', end='')
+print(f'\nO valor 9 apareceu {num.count(9)} veze(s)')# mostra a contagem de quantas vezes o numero 9 apareceu
+if 3 in num:
+    print(f'A posição do primeiro valor 3 foi {num.index(3)+ 1}° posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
+print('Os valores pares digitados foram ', end='')
+for n in num:
+    if n % 2 == 0:
+        print(n, end=' ')
