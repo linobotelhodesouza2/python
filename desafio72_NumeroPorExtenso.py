9ce4955689f6b488669811f6d57dbb562996b942
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-10-19 13:09:21 -0300

    Add files via upload

--- desafio72_NumeroPorExtenso.py
+++ desafio72_NumeroPorExtenso.py
@@ -3,6 +3,7 @@ CRIE UM PROGRAMA QUE TENHA UMA TUPLA TOTALMENTE PREENCHIDA COM UMA CONTAGEM POR 
 VINTE.
 SEU PROGRAMA DEVERÁ LER UM NUMERO PELO TECLADO(ENTRE 0 E 20) E MOSTRÁ-LO POR EXTENSO.
 '''
+from time import sleep
 numeros = (
 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
 'catorze', 'quinze', 'desesseis', 'desessete', 'dezoito', 'dezenove', 'vinte')
@@ -10,6 +11,12 @@ numeros = (
 while True:
     num = int(input('Digite um Número entre 0 e 20: '))
     if 0 <= num <= 20:
-        break
-    print('Tente novamente', end= '')
-print(f'Voçê digitou o numero {numeros[num]}')
+        print(f'Voçê digitou o numero {numeros[num]}')
+        usuario = str(input('Voçê deseja continuar? S/N ')).upper().strip()[0]
+        if usuario == 'S':
+            print('', end=' ')# se na digitar o numero dentro do while ele coloca o tente novamente sem pular linha com o end=''
+        else:
+            print('Finalizando o programa aguarde...')
+            time = sleep(4)
+            print('\nPrograma Finalizado!')
+            break
